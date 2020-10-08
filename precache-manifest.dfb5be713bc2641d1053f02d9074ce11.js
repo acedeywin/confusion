05ff7c238184a88a62482bae9a2301603ed0d442
Stephen Okpalaononuju <stephenokpala@gmail.com>
2020-10-08 18:04:41 +0100

    Updates

---
+++ precache-manifest.dfb5be713bc2641d1053f02d9074ce11.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf7c3d279e395030fe39d5aea88a3bfd",
+    "url": "/confusion/index.html"
+  },
+  {
+    "revision": "dbe3e0ef598bd470bb2a",
+    "url": "/confusion/static/css/2.df694ce1.chunk.css"
+  },
+  {
+    "revision": "852103436dbc3b90a040",
+    "url": "/confusion/static/css/main.23e744cf.chunk.css"
+  },
+  {
+    "revision": "dbe3e0ef598bd470bb2a",
+    "url": "/confusion/static/js/2.781a6588.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/confusion/static/js/2.781a6588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "852103436dbc3b90a040",
+    "url": "/confusion/static/js/main.ae028595.chunk.js"
+  },
+  {
+    "revision": "3a2e8a75e423d2f0420b",
+    "url": "/confusion/static/js/runtime-main.beb46ec9.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
